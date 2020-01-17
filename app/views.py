712c14b9dc0a7843bc01953b400eec67d8f81b59
Andrej Ruman <adorum@gmail.com>
2020-01-17 14:20:40 +0100

    order

--- app/views.py
+++ app/views.py
@@ -5,5 +5,5 @@ from app.models import RealityPost
 class RealityPostList(ListView):
     model = RealityPost
     paginate_by = 30
-    queryset = RealityPost.objects.order_by('-date_updated', '-date_created')
+    queryset = RealityPost.objects.order_by('-date_updated')
     template_name = 'app/realitypost_list.html'
