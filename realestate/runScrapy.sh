d37a1b78c0324dc6f5ba51440aa4547c9de56820
Andrej Ruman <adorum@gmail.com>
2020-01-26 17:37:24 +0100

    update script

--- realestate/runScrapy.sh
+++ realestate/runScrapy.sh
@@ -2,4 +2,4 @@
 
 source $HOME/scrapy/bin/activate
 cd $HOME/reality/realestate
-scrapy crawl topreality && scrapy crawl nehnutelnosti && scrapy crawl byty
+scrapy crawl topreality && scrapy crawl nehnutelnosti && scrapy crawl byty && scrapy crawl bazos
