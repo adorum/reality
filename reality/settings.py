4460f31e97436b84e295568344a22f721025332e
Andrej Ruman <adorum@gmail.com>
2020-09-27 20:54:04 +0200

    Change sqlite to postgres

--- reality/settings.py
+++ reality/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'reality.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'reality',
+        'USER': 'reality',
+        'PASSWORD': 'reality',
+        'HOST': os.getenv('DATABASE_SERVER_URL', 'localhost'),
     }
 }
 
--- reality_worker/tasks.py
+++ reality_worker/tasks.py
@@ -1,13 +1,16 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-import os
 from reality_worker.celery import app
 from reality_scrapy.reality_scrapy.spiders.topreality import ToprealitySpider
+from reality_scrapy.reality_scrapy.spiders.nehnutelnosti import NehnutelnostiSpider
+from reality_scrapy.reality_scrapy.spiders.bazos import BazosSpider
 
 
 @app.task
 def create_random_user_accounts():
     process = CrawlerProcess(get_project_settings())
     process.crawl(ToprealitySpider)
+    process.crawl(NehnutelnostiSpider)
+    process.crawl(BazosSpider)
     process.start()
     return 0
